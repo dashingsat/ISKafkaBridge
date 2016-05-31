28626151d754396b9fda8e256a17a627b447b1b5
dashingsat <satyabrata1.dash@gmail.com>
2016-05-31 11:14:31 +0530

    Bootstrap

---
+++ src/com/gcs/lib/ISKafkaIntegration/IDataSerializer.java
@@ -0,0 +1,7 @@
+package com.gcs.lib.ISKafkaIntegration;
+
+/**
+ * Created by sdas on 5/31/2016.
+ */
+public class IDataSerializer {
+}
---
+++ src/com/gcs/lib/ISKafkaIntegration/KafkaProducer.java
@@ -0,0 +1,11 @@
+package com.gcs.lib.ISKafkaIntegration;
+
+/**
+ * Created by sdas on 5/31/2016.
+ */
+public class KafkaProducer {
+
+
+
+
+}
